3938bdce16fa13cc552fac3302e40cf57a140c2e
goliatone <burgosemi@gmail.com>
2015-04-11 22:12:56 -0400

    TEST: WIP

--- tests/librarian_test.js
+++ tests/librarian_test.js
@@ -46,6 +46,36 @@ describe('Librarian', function(){
         });
     });
 
+    describe('#source', function(){
+        it('should set a "source" directory in DEFAULTS', function(){
+            var l = Librarian();
+            assert.ok(l.source(), Librarian.DEFAULTS.source);
+        });
+
+        it('should set the "source" directory', function(){
+            var l = Librarian({source:'dir'});
+            assert.ok(l.directory(), 'dir');
+        });
+
+        it('should get full path of source directory', function(){
+            var l = Librarian();
+            var absoluteBuildPath = path.join(process.env.PWD, '/build');
+            assert.ok(l.source(), absoluteBuildPath);
+        });
+
+        it('should respect absolute directories', function(){
+            var l = Librarian({source:'/dir'});
+            assert.ok(l.source(), '/dir');
+        });
+
+        it('should throw on non strings', function(){
+            var l = Librarian();
+            assert.throws(function(){
+                l.source(23);
+            });
+        });
+    });
+
     describe('#destination', function(){
         it('should set a destination directory in DEFAULTS', function(){
             var l = Librarian();
@@ -68,6 +98,84 @@ describe('Librarian', function(){
             assert.throws(function(){
                 l.destination(23);
             });
+        });
+    });
+
+    describe('#clean', function(){
+        it('should set the "clean" default used in DEFAULTS', function(){
+            var l = Librarian();
+            assert.equal(l.clean(), Librarian.DEFAULTS.clean);
+        });
+
+        it('should set the "clean" option', function(){
+            var l = Librarian();
+            l.clean(false);
+            assert.equal(l.clean(), false);
+        });
+
+        it('should throw on on boolean', function(){
+            var l = Librarian();
+            assert.throws(function(){
+                l.clean(23);
+            });
+        });
+    });
+
+    describe('#frontmatter', function(){
+        it('should set the "frontmatter" default used in DEFAULTS', function(){
+            var l = Librarian();
+            assert.equal(l.frontmatter(), Librarian.DEFAULTS.frontmatter);
+        });
+
+        it('should set the "frontmatter" option', function(){
+            var l = Librarian({frontmatter: true});
+            assert.equal(l.frontmatter(), true)
+        });
+
+        it('should set the "frontmatter"', function(){
+            var l = Librarian();
+            l.frontmatter(true);
+            assert.equal(l.frontmatter(), true)
+        });
+
+        it('should throw on on boolean', function(){
+            var l = Librarian();
+            assert.throws(function(){
+                l.frontmatter(23);
+            });
+        });
+    });
+
+    describe('#metadata', function(){
+        it('should set the "metadata" default used in DEFAULTS', function(){
+            var l = Librarian();
+            assert.deepEqual(l.metadata(), Librarian.DEFAULTS.metadata);
+        });
+
+        it('should set the "metadata" option', function(){
+            var meta = {
+                user: 'goliatone',
+                url:'http://goliatone.com'
+            };
+
+            var l = Librarian({metadata: meta});
+            assert.deepEqual(l.metadata(), meta);
+        });
+
+        it('should get "metadata"', function(){
+            var l = Librarian();
+            assert.deepEqual(l.metadata(), {});
+        });
+
+        it('should set "metadata" clone', function(){
+            var meta = {
+                user: 'goliatone',
+                url:'http://goliatone.com'
+            };
+            var l = Librarian();
+            l.metadata(meta);
+            assert.notEqual(l.metadata(), meta);
+            assert.deepEqual(l.metadata(), meta);
         });
     });
 });
